17a0726122822ea8f5c2c3337b52cba3581a9cad
Valery Varaksa <varaksav62@gmail.com>
2025-06-22 20:16:56 +0300

    Implement StringBuilderPool

---
+++ ApplicationCore/Utilities/StringBuilderPool.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+
+namespace ApplicationCore.Utilities;
+
+public static class StringBuilderPool
+{
+    private static readonly Queue<StringBuilder> Pool;
+
+    static StringBuilderPool()
+    {
+        Pool = new Queue<StringBuilder>();
+        Pool.Enqueue(new StringBuilder());
+        Pool.Enqueue(new StringBuilder());
+        Pool.Enqueue(new StringBuilder());
+    }
+    
+    public static StringBuilder Rent()
+    {
+        return Pool.Count > 0 ? Pool.Dequeue() : new StringBuilder();
+    }
+
+    public static void Return(StringBuilder sb)
+    {
+        sb.Clear();
+        Pool.Enqueue(sb);
+    }
+}
