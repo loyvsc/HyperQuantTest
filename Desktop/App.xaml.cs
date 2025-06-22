8ab2787a6db9c5cd80fc695752dc785d9667203c
Valery Varaksa <varaksav62@gmail.com>
2025-06-22 22:45:54 +0300

    Implement DI

--- Desktop/App.xaml.cs
+++ Desktop/App.xaml.cs
@@ -1,6 +1,7 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Windows;
+﻿using System.Windows;
+using ApplicationCore.Interfaces;
+using DAL.Connectors;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Desktop;
 
@@ -9,4 +10,34 @@ namespace Desktop;
 /// </summary>
 public partial class App : Application
 {
+    private IServiceProvider _serviceProvider;
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        var serviceCollection = new ServiceCollection();
+        ConfigureServices(serviceCollection);
+
+        _serviceProvider = serviceCollection.BuildServiceProvider();
+        
+        Exit += OnExit;
+
+        var mainWindow = new MainWindow();
+        mainWindow.Show();
+    }
+
+    private void ConfigureServices(IServiceCollection services)
+    {
+        services.AddLogging();
+        
+        services.AddSingleton<ITestConnector, BitfinexConnector>();
+    }
+
+    private void OnExit(object sender, ExitEventArgs e)
+    {
+        Exit -= OnExit;
+        if (_serviceProvider is IDisposable disposable)
+        {
+            disposable.Dispose();
+        }
+    }
 }
