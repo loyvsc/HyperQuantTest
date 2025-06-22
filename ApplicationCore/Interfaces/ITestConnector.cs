6ec6759ad422343869cabab75c0de16b72dbc240
Valery Varaksa <119746866+loyvsc@users.noreply.github.com>
2025-06-22 21:52:05 +0300

    Implement BitfinexConnector.GetTickerAsync (#2)

--- ApplicationCore/Interfaces/ITestConnector.cs
+++ ApplicationCore/Interfaces/ITestConnector.cs
@@ -9,6 +9,7 @@ public interface ITestConnector : IDisposable
 
     Task<IEnumerable<Trade>> GetNewTradesAsync(string pair, int maxCount);
     Task<IEnumerable<Candle>> GetCandleSeriesAsync(string pair, int periodInSec, DateTimeOffset? from, DateTimeOffset? to = null, long? count = 0);
+    Task<Ticker?> GetTickerAsync(string pair);
 
     #endregion
 
